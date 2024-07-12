8fbfc83d0e7c731d2d121168949f35487de0a22f
Daniel Nwafor <danidev3lops@gmail.com>
2024-07-12 04:08:21 +0000

    add the Users and Products pages and subpages

--- app/dashboard/products/add/page.tsx
+++ app/dashboard/products/add/page.tsx
@@ -1,5 +1,29 @@
-import React from "react";
+import styles from "@/components/dashboard/products/add-product/addProduct.module.css";
 
 export default function AddProduct() {
-  return <div>AddProduct</div>;
+  return (
+    <div className={styles.container}>
+      <form className={styles.form}>
+        <input type="text" placeholder="title" name="title" required />
+        <select name="cat" id="cat">
+          <option value="general">Choose a Category</option>
+          <option value="kitchen">Kitchen</option>
+          <option value="phone">Phone</option>
+          <option value="computer">Computer</option>
+        </select>
+        <input type="number" placeholder="price" name="price" required />
+        <input type="number" placeholder="stock" name="stock" required />
+        <input type="text" placeholder="color" name="color" />
+        <input type="text" placeholder="size" name="size" />
+        <textarea
+          required
+          name="desc"
+          id="desc"
+          rows={16}
+          placeholder="Description"
+        ></textarea>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
